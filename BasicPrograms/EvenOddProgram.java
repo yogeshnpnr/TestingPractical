eb4538f0edbdc37535446fa27019078065afd519
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2023-05-10 19:45:57 +0530

    Create EvenOddProgram.java

---
+++ BasicPrograms/EvenOddProgram.java
@@ -0,0 +1,16 @@
+public class EvenOddProgram {
+
+
+    public static void main(String[]args){
+
+        int i =311;
+        if(i%2==0){
+
+            System.out.println( i+" it is a even number" );
+        }
+        else{
+            System.out.println( i+" it is a odd number" );
+
+        }
+    }
+}
