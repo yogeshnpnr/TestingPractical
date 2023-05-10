ee1d98593bb44ffbe3195faa9ebfbe570d7e9ede
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2023-05-10 20:48:38 +0530

    utils

---
+++ MobileFrameworrk/utils/Constant.java
@@ -0,0 +1,13 @@
+package utility;
+
+
+public class Constant {
+	public static String Path_TestData="";
+	public static String Path_Test = "";
+	public static String username="QA_CLIENT";
+	public static String password="abc123";
+	public static String qa1="1";
+	public static String qa2="1";
+	public static String graph_heigh="304";
+	public static String graph_width="227";
+}
