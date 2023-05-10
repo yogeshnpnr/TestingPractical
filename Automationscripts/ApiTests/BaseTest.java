81a72502b4993d10fa279674830705b12848d248
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2023-05-10 20:22:51 +0530

    BaseTest

---
+++ Automationscripts/ApiTests/BaseTest.java
@@ -0,0 +1,32 @@
+package APItests;
+
+import static io.restassured.config.LogConfig.logConfig;
+import static io.restassured.config.RestAssuredConfig.newConfig;
+
+import org.testng.annotations.BeforeMethod;
+
+import com.testingsumo.endpoints.APIConstants;
+import com.testingsumo.payload.RequestPayloads;
+
+import actions.APIActions;
+import actions.AssertActions;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseTest {
+
+	protected RequestSpecification repoSpec;
+	protected AssertActions assertActions = new AssertActions();
+	protected APIActions apiActions = new APIActions();
+	protected RequestPayloads requestPayload = new RequestPayloads();
+
+	@BeforeMethod
+
+	protected void setUpConfiguration() {
+
+		repoSpec = new RequestSpecBuilder().setBaseUri(APIConstants.baseUrl)
+				.setConfig(newConfig().logConfig(logConfig().blacklistHeader("Authorization")))
+				.addHeader("Authorization", APIConstants.token).setAccept("application/json")
+				.setContentType("application/json").build().log().all();
+	}
+}
