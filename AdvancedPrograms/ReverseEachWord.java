8112d0638a9c3c5b6b4b314c2a6d0dbe3e1ce5c0
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-13 00:04:02 +0530

    Update ReverseEachWord.java

--- AdvancedPrograms/ReverseEachWord.java
+++ AdvancedPrograms/ReverseEachWord.java
@@ -1,22 +1,25 @@
-public class ReverseEachWord {
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class ReverseEachWord {
     public static void main(String[] args) {
+        
         String str = "HELLO YOGESH";
-        String[] words = str.split(" ");
-        String result = "";
-
-        for (String word : words) {
-            result += reverseString(word) + " ";
+       String[] words = str.split(" ");
+       String result = "";
+       
+       
+    for (String word : words) {
+            result =result+ reverseString(word) + " ";
         }
-
-        System.out.println("Reversed words: " + result.trim());
-    }
 
     // Method to reverse a string manually
     public static String reverseString(String word) {
         char[] chars = word.toCharArray();
         String reversed = "";
         for (int i = chars.length - 1; i >= 0; i--) {
-            reversed += chars[i];
+            reversed = reversed+chars[i];
+          
         }
         return reversed;
     }
