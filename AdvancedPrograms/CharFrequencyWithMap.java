6bedde33264669b19dffb75579741c61fc9f9151
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-13 15:01:44 +0530

    Create CharFrequencyWithMap.java

---
+++ AdvancedPrograms/CharFrequencyWithMap.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharFrequencyWithMap {
+    public static void main(String[] args) {
+        String str = "HELLO INDIA";
+        printCharFrequency(str);
+    }
+
+    public static void printCharFrequency(String str) {
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+        // Iterate through the string and count occurrences
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ') continue; // Ignore spaces
+
+            // Increment count in the map
+            if (charCountMap.containsKey(ch)) {
+                charCountMap.put(ch, charCountMap.get(ch) + 1);
+            } else {
+                charCountMap.put(ch, 1);
+            }
+        }
+
+        // Print character frequencies
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
