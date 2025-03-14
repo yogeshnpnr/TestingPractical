d7797dcb9d0235a438b96bee802f76410a33b96e
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-14 22:53:05 +0530

    Update AdvancedPrograms

---
+++ AdvancedPrograms/Armstrong.java
@@ -0,0 +1,22 @@
+public class ArmstrongNumber {
+
+
+    public static void main(String[] args) {
+
+        int number = 371, sum = 0, rem;
+        int temp = number;
+
+        while (number > 0) {
+            rem = number % 10;// 153%10= 3   15%10= 5
+            number = number / 10; //153/10 = 15  15/10 =1
+            sum = sum + (rem * rem * rem);// 0+(3*3*3)=27
+
+        }
+        if (sum == temp) {
+            System.out.println("it is Armstrong number");
+        } else {
+            System.out.println("it is not Armstrong number");
+
+        }
+    }
+}
