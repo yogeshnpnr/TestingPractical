819efbcbf678e81f26b9a2297c2fb20042ad95a5
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-14 23:34:31 +0530

    Update FindSpecialChars.java

--- AdvancedPrograms/FindSpecialChars.java
+++ AdvancedPrograms/FindSpecialChars.java
@@ -1,12 +1,15 @@
-public class FindSpecialChars {
-    public static void main(String[] args) {
-        String str = "Hello@World#2024!";
-        System.out.print("Special Characters: ");
-
+public class FindSpecialChars  {
+    public static void findSpecialChars(String str) {
+        System.out.print("Special characters: ");
         for (char ch : str.toCharArray()) {
-            if (!Character.isLetterOrDigit(ch) && !Character.isWhitespace(ch)) {
+            if (!Character.isLetterOrDigit(ch)) {
                 System.out.print(ch + " ");
             }
         }
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello, World! #2025@";
+        findSpecialChars(str);
     }
 }
