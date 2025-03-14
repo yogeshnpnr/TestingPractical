27435ab9e476b95b2a21227367b9f001ec5b276e
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-14 16:55:19 +0530

    Create Fibonaccinumber.java

---
+++ AdvancedPrograms/Fibonaccinumber.java
@@ -0,0 +1,18 @@
+public class Fibonaccinumber {
+
+    public static void main(String[]args){
+
+        int a=0, b=1, sum, count=5;
+
+        System.out.print(a+","+b);
+
+        for (int i=2;i<=count;i++){
+            sum =  a+b;
+            a=b;
+            b=sum;
+            System.out.print(","+ sum );
+
+        }
+
+    }
+}
