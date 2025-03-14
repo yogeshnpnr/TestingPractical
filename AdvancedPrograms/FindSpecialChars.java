9e893230effd46f99199ef53fa78ff4250e12290
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-14 23:28:57 +0530

    Update and rename SpecialCharsFind.java to FindSpecialChars.java

--- AdvancedPrograms/FindSpecialChars.java
+++ AdvancedPrograms/FindSpecialChars.java
@@ -1,4 +1,4 @@
-public class SpecialCharsFind {
+public class FindSpecialChars {
     public static void main(String[] args) {
         String str = "Hello@World#2024!";
         System.out.print("Special Characters: ");
