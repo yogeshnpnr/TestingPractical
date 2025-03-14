3b76e3ab8acbcc1ab9b4a969d1974c19d4ed6ead
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-14 16:56:15 +0530

    Create PalindromNumber.java

---
+++ AdvancedPrograms/PalindromNumber.java
@@ -0,0 +1,28 @@
+public class PalindromNumber {
+
+    public static void main(String[] args) {
+        int rem, sum = 0, temp;
+        int num = 1234;
+        temp = num;
+        while (num > 0) {
+
+            rem = num % 10;
+            num = num / 10;
+            sum = (sum * 10) + rem;
+
+        }
+
+        System.out.println(sum);
+
+        if (temp == sum) {
+
+            System.out.println("it is palindrome number");
+
+        } else {
+            System.out.println(" it is not palindrome number");
+
+        }
+    }
+
+
+}
