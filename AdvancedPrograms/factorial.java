3ba594332f83036cfc5812dd9766df1617ac3446
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-14 22:54:47 +0530

    Update AdvancedPrograms

---
+++ AdvancedPrograms/factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+
+    public static void main(String []args){
+
+        int number=5; int fact =1;
+
+        for ( int i = 1; i<=number; i++){
+
+            fact = fact*i;
+            System.out.println( "factorial of 5 is "+fact);
+
+        }
+
+    }
+
+}
