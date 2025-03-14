92b529c1237c8f8f4eba7abe3bdf48db4237e7c3
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-14 23:26:49 +0530

    Create StringAngaramp.java

---
+++ AdvancedPrograms/StringAngaramp.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class StringAngaramp {
+
+    public static void main(String []args ){
+
+        String str1 = "tops";
+        String str2 = "stop";
+
+
+       // String str = "army";
+        //String str2 ="mary";
+
+        char[] arr = str1.toLowerCase().toCharArray();
+        char[] arr2= str2.toLowerCase().toCharArray();
+
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+
+        if(Arrays.equals(arr,arr2)){
+            System.out.println("they are anagram");
+        } else{
+            System.out.println("they are not");
+        }
+
+
+//
+//        char[] arr3 = str1.toLowerCase().toCharArray();
+//        char[] arr4 =str2.toLowerCase().toCharArray();
+//
+//        Arrays.sort(arr3);
+//        Arrays.sort(arr4);
+//
+//        if (Arrays.equals(arr3,arr4)){
+//            System.out.println("they are anagram");
+//        } else{
+//            System.out.println("they are not");
+//
+//        }
+
+
+    }
+
+
+    }
