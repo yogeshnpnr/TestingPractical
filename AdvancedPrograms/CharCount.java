489f262fba9f85bf07bdc1572e5eccba01e5b1c4
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-14 17:08:46 +0530

    Create CharCount.java

---
+++ AdvancedPrograms/CharCount.java
@@ -0,0 +1,7 @@
+public class CharCount {
+    public static void main(String[] args) {
+        String str = "HELLO";
+        int count = str.length();
+        System.out.println("Total characters in the string: " + count);
+    }
+}
