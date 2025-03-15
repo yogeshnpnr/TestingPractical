4abb4716e44d846d447a5de072f914871b282a08
yogeshnpnr <133116742+yogeshnpnr@users.noreply.github.com>
2025-03-15 14:50:59 +0530

    Update ConsecutiveCharacters.java

--- AdvancedPrograms/ConsecutiveCharacters.java
+++ AdvancedPrograms/ConsecutiveCharacters.java
@@ -1,19 +1,21 @@
 public class ConsecutiveCharacters {
-
-   public static void main(String[] args) {
-        String input = "abbcddeff";
+    public static void main(String[] args) {
+        String input = "aabcfffdjkloo";
         findConsecutiveChars(input);
     }
-  
+
     public static void findConsecutiveChars(String str) {
         int n = str.length();
 
         for (int i = 0; i < n - 1; i++) {
             if (str.charAt(i) == str.charAt(i + 1)) {
-                System.out.println("Consecutive chars: " + str.charAt(i) + str.charAt(i + 1) + " at index " + i);
+                System.out.println("Consecutive chars: " + str.charAt(i) + str.charAt(i + 1));
+
+                // Skip over the entire consecutive sequence
+                while (i < n - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                    i++;
+                }
             }
         }
     }
-
-   
 }
